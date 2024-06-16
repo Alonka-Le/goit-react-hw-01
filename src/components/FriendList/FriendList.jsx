fb1ecf12abeb505cdaad171a77c59a27b639a3b1
Alonka-Le <alyona.sholohowa120@gmail.com>
2024-06-16 22:09:45 +0200

    FriendListItem

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,4 +1,4 @@
-import FriendListItem from './FriendListItem';
+import FriendListItem from '../FriendListItem/FriendListItem';
 import css from '../FriendList/FriendList.module.css';
 
 export default function FriendList({ friends }) {
